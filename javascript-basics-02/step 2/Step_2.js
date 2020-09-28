fd040813b16281e49dc3afee66d7f3b3a1745325
ahazimeh <hazimeh95@gmail.com>
2020-09-28 12:46:49 +0300

    Step1.js+Step1.html

---
+++ javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,8 @@
+var img = document.getElementById("name");
+// myScript.src="images/image1_2.jpg";
+// var att = document.createAttribute("onmouseover");
+// att.value="myScript";
+// object.addEventListener("mouseover", myScript);
+img.addEventListener("onblur",function(){
+    alert("hi");
+});
