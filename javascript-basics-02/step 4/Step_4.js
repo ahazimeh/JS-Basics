cb1018b2a65dcc07e06c50520ecab8e37b07e2ef
ahazimeh <hazimeh95@gmail.com>
2020-09-29 09:19:31 +0300

    Step_4.js

---
+++ javascript-basics-02/step 4/Step_4.js
@@ -0,0 +1,11 @@
+
+
+
+var button = document.getElementsByName("button");
+button[0].addEventListener("click",function(){
+    document.getElementById("name").value='';
+    document.getElementById("surname").value='';
+    document.getElementById("city").value='';
+    confirm("asd");
+});
+
